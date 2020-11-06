b7d5b202c12a3e00b3cdba11cb671bfe38848701
jalynee <hyuns0203@gmail.com>
2020-11-06 10:12:54 +0900

    리액트 무비 앱 업로드

---
+++ src/routes/Home.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+class Home extends React.Component {
+  state = {
+    isLoading: true, //key : key value
+    movies : [],
+  }
+
+  getMovies = async () =>{
+    const {
+      data: {
+        data: {movies},
+      },
+     } = await axios.get('https://yts-proxy.now.sh/list_movies.json?genre=animation&sort_by=like_count');
+    // console.log(movies);
+    // this.setState({movies: movies, isLoading: false}); 
+    this.setState({movies, isLoading: false}); //키 : 키값 이름이 동일하면
+  }
+
+  componentDidMount () {
+    // setTimeout(() => {
+    //   this.setState({isLoading:false});
+    // },6000)
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading, movies} = this.state; //구조 분해 할당
+    
+    return (
+      <section className="container">
+        {isLoading ? 
+        (<div className="loader">
+          <span className="loader_text">'Loading...'</span>
+        </div>) : 
+        (<div className="movies">
+          {movies.map(movie => ( <Movie
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                              /> )
+                      )                   
+          }
+        </div>)
+        }
+      </section>    
+    );
+  }
+}
+
+export default Home;
